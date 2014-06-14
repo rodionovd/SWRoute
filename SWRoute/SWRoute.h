38cdf442943a305d22663aa3e3334db4f4709219
rodionovd <i.am.rodionovd@gmail.com>
2014-06-15 04:00:15 +0700

    Initial Commit

---
+++ SWRoute/SWRoute.h
@@ -0,0 +1,19 @@
+//
+//  SWRoute.h
+//  SWRoute
+//
+//  Created by Dmitry Rodionov on 6/15/14.
+//  Copyright (c) 2014 rodionovd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SWRoute.
+FOUNDATION_EXPORT double SWRouteVersionNumber;
+
+//! Project version string for SWRoute.
+FOUNDATION_EXPORT const unsigned char SWRouteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWRoute/PublicHeader.h>
+
+
