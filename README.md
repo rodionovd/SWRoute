0391c896158d0f7ec32d984dc7e62641af3b7490
rodionovd <i.am.rodionovd@gmail.com>
2014-06-19 16:58:17 +0700

    Add license information: WTFPL

--- README.md
+++ README.md
@@ -80,7 +80,19 @@ class EvilRoute {
         return Int(err)
     }
 }
+```  
+
+### License
+
+[WTFPL](http://www.wtfpl.net/).  
+
+```cpp
+//  Copyright © 2014 Dmitry Rodionov <i.am.rodionovd@gmail.com>
+//  This work is free. You can redistribute it and/or modify it under the
+//  terms of the Do What The Fuck You Want To Public License, Version 2,
+//  as published by Sam Hocevar. See the COPYING file for more details.
 ```
+ 
 
 ------
 
--- SWRoute/SWRoute.h
+++ SWRoute/SWRoute.h
@@ -1,10 +1,11 @@
 //
 //  SWRoute.h
 //  SWRoute
-//
-//  Created by Dmitry Rodionov on 6/15/14.
-//  Copyright (c) 2014 rodionovd. All rights reserved.
-//
+//  
+//  Copyright © 2014 Dmitry Rodionov <i.am.rodionovd@gmail.com>
+//  This work is free. You can redistribute it and/or modify it under the
+//  terms of the Do What The Fuck You Want To Public License, Version 2,
+//  as published by Sam Hocevar. See the COPYING file for more details.
 
 #import <Cocoa/Cocoa.h>
 
